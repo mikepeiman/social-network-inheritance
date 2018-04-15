ec2c4c739f0d465c29f3ba50db82f90dd07d5561
mikepeiman <mikepeiman@gmail.com>
2018-04-15 16:54:04 +0000

    Getting started on test_posts.py, first test passed.

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,33 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None):
+        pass
+
+    def set_user(self, user=None):
+        self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+        pass
+
+    def __str__(self):
+        pass
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        pass
+
+    def __str__(self):
+        pass
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        pass
+
+    def __str__(self):
+        pass
