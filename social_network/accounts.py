bcb0a1fbbfc6e0998e7ea2114c395aec72f7d85b
mikepeiman <mikepeiman@gmail.com>
2018-04-15 19:23:59 +0000

    6 of 7 test passing, one remains, sorting timeline list

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -4,12 +4,22 @@ class User(object):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.following = []
+        self.posts = []
 
     def add_post(self, post):
-        pass
+        post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
+        timeline = []
+        for user in self.following:
+            timeline.append(user.posts)
+        timeline.append(self.posts)
+        return sorted(timeline, key=lambda p: p.timestamp, reverse=False)
 
     def follow(self, other):
-        pass
+        self.following.append(other)
+        
+        
+        
